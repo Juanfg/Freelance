d42f6b15da67eb041d6e95248bfb35b9558ae711
Juan Pablo Flores Galindo <juanfg@outlook.com>
2017-05-07 18:34:20 -0500

    Added the HELP functionality in the current views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,36 @@
 @extends('layouts.sidebar')
 
+@section('title', 'Home')
+
 @section('content')
-<div class="row fix">
+<div class="row fix" data-step="1" data-intro="This is the home page. You can see all the projects here!" data-position='top'>
 	@foreach($projects as $project)
 	<div class="col-xs-3">
-		<div class="card card-mini">
+		<div class="card card-mini"  data-step="2" data-intro="This is a single project" data-position='top'>
 			<div class="card-header">
 				<!-- Aquí iría la imagen -->
 			</div>
-			<div class="card-body-title style="display: inline-block;">
+			<div class="card-body-title style=" display: "inline-block;" data-step="3" data-intro="This is the name of the project" data-position='top'>
 				{{$project->name}}
 			</div>
-			<div class="card-body-description">
+			<div class="card-body-description" data-step="4" data-intro="This is name of the owner of the project" data-position='top'>
 				Owner: {{$project->owner()->first()->name}}
 			</div>
-			<div class="card-body-description">
+			<div class="card-body-description" data-step="5" data-intro="This is how difficult is the project from 1 to 10" data-position='top'>
 				Difficulty: {{$project->difficulty}}
 			</div>
-			<div class="card-body-description">
+			<div class="card-body-description" data-step="6" data-intro="These are the categories of the project" data-position='top'>
 				@foreach ($project->categories()->get() as $category)
 				<button type="button" class="btn btn-primary btn-xs">{{$category->name}}</button>
 				@endforeach
 			</div>
 			<div class="card-body">
-				<button type="button" class="btn btn-success">See more</button>
+				<button type="button" class="btn btn-success"  data-step="7" data-intro="With this button you can have a better perspective of the project" data-position='top'>
+					See more
+				</button>
 			</div>
 		</div>
-		</div>
+	</div>
 	@endforeach
 </div>
 @endsection
